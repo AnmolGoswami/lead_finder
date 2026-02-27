bea0dba199f052251b09601e947f6699a100f2ba
Anmol Giri <techsplit858@gmail.com>
2026-02-27 18:25:57 +0530

    This is the lead finder app

---
+++ src/main/java/com/lead_finder/LeadFinderApplication.java
@@ -0,0 +1,13 @@
+package com.lead_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadFinderApplication.class, args);
+	}
+
+}
